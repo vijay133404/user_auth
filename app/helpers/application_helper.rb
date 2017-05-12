9772e946d90b1368a89ecdb69e5c57055d27910e
vijay133404 <vmaurya1989@gmail.com>
2017-05-12 18:32:50 +0530

    applications helper using

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+    def user_permission_admin?
+       if current_user
+         UserRole.find(current_user.role_id).role_name == "Admin"
+       else
+       false
+       end
+    end
+
+    def user_permission_manager?
+       if current_user
+         UserRole.find(current_user.role_id).role_name == "Manager"
+       else
+       false
+       end
+    end
+
+    def user_permission_user?
+       if current_user
+         UserRole.find(current_user.role_id).role_name == "User"
+       else
+       false
+       end
+    end
 end
+
+
