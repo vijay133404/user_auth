79c43fff2a312266dfdbc093d621675f24bcbd69
vijay133404 <vmaurya1989@gmail.com>
2017-05-15 11:48:14 +0530

    helper changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,41 +2,34 @@
 module ApplicationHelper
     def user_permission_admin?
        if current_user
-         UserRole.find(current_user.role_id).role_name == "Admin"
+         UserRole.find(current_user.role_id).role_name == "admin"
        else
        false
        end
     end
 
-    def user_permission_manager?
-       if current_user
-         UserRole.find(current_user.role_id).role_name == "Manager" && User.all.map(&:company_id) == current_user.company_id
-       else
-           false
-       end
-    end
+    
 
     def user_permission_user?
        if current_user
-         UserRole.find(current_user.role_id).role_name == "User"
+         UserRole.find(current_user.role_id).role_name == "user"
        else
        false
        end
     end
 
-    def user_check(user1, curr_user)
-        if UserRole.find(curr_user.role_id).role_name == "Manager"
-            if  user1.company_id == curr_user.company_id
+    def user_check(manage, curr_user)
+        if UserRole.find(curr_user.role_id).role_name == "manager"
+            if  manage.company_id == curr_user.company_id
               true
             else
               false
             end
-        elsif UserRole.find(curr_user.role_id).role_name == "Admin"
+       
 
 
         else
-
-
+        false
         end
     end
 end
